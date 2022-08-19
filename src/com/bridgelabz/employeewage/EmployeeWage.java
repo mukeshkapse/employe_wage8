b8c207c631dfb4e4e4ec0af68eb6f202af9b3ef7
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-20 00:48:56 +0530

    Added: Employee Wage Solving Switch Case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -11,15 +11,17 @@ public class EmployeeWage
    static void parttimewage(){
        System.out.println("Employee Part time wage os "+ wage_per_houre*part_time_houre);
    }
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome To Emplyee Wage Computation Program ");
-        double empstatus = Math.floor(Math.random()*10) %2;
-        if(empstatus==0) {
-            System.out.println("empoyee is Prasent ");
-        }else {
+        double empstatus = Math.floor(Math.random() * 10) % 2;
+        switch (empstatus){
+        case 1:
+        System.out.println("empoyee is Prasent ");
+        break;
+        case 2:
         System.out.println("employee is Absent ");
-        }
+        break;
+         }
         dailywage();
         parttimewage();
     }
