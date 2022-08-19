ad3e442743f98c050231e9ad0d7b33f6bddded59
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-20 00:38:13 +0530

    Added : Employee part time wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.employeewage;
 
-public class EmployeeWage<wage_per_houre>
+public class EmployeeWage
 {
     static final int wage_per_houre = 20;
     static final int full_day = 8;
-    static void dailywage(){
-        System.out.println("Employee Wage is "+ wage_per_houre*full_day);
-    }
+    static final int part_time_houre = full_day/2;
+   static void dailywage(){
+       System.out.println("Employee Wage is "+ wage_per_houre*full_day);
+   }
+   static void parttimewage(){
+       System.out.println("Employee Part time wage os "+ wage_per_houre*part_time_houre);
+   }
     public static void main(String[] args)
     {
         System.out.println("Welcome To Emplyee Wage Computation Program ");
@@ -17,5 +21,6 @@ public class EmployeeWage<wage_per_houre>
         System.out.println("employee is Absent ");
         }
         dailywage();
+        parttimewage();
     }
 }
