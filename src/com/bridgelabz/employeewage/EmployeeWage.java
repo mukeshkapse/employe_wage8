54a8b5afab6beee0c3626371ab974366181080e6
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-20 01:37:45 +0530

    Added: Add Part time wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -2,27 +2,28 @@ package com.bridgelabz.employeewage;
 
 public class EmployeeWage
 {
-    static final int wage_per_houre = 20;
-    static final int full_day = 8;
-    static final int part_time_houre = full_day/2;
-   static void dailywage(){
-       System.out.println("Employee Wage is "+ wage_per_houre*full_day);
-   }
-   static void parttimewage(){
-       System.out.println("Employee Part time wage os "+ wage_per_houre*part_time_houre);
-   }
-    public static void main(String[] args) {
-        System.out.println("Welcome To Emplyee Wage Computation Program ");
-        double empstatus = Math.floor(Math.random() * 10) % 2;
-        switch (empstatus){
-        case 1:
-        System.out.println("empoyee is Prasent ");
-        break;
-        case 2:
-        System.out.println("employee is Absent ");
-        break;
-         }
-        dailywage();
-        parttimewage();
-    }
-}
+        static final int wage_per_houre = 20;
+        static final int full_day = 8;
+        static final int part_time_houre = full_day/2;
+        static final int working_day_Per_month = 20;
+
+        public static void main(String[] args)
+        {
+            System.out.println("Welcome To Emplyee Wage Computation Program ");
+            int EmpIsPrasentFullTime = 1;
+            int EmpIsPrasentHalfTime = 2;
+
+            double empstatus = Math.floor(Math.random() * 10) % 2;
+            if (empstatus == EmpIsPrasentFullTime)
+            {
+                System.out.println("Employee full day Wage " + wage_per_houre * full_day);
+            } else if(empstatus==EmpIsPrasentHalfTime)
+            {
+                System.out.println("Employee Part time wage is "+ wage_per_houre*part_time_houre);
+            }
+            else{
+                System.out.println("Employee is absent");
+            }
+
+        }}
+
