3bd380149f8b84939542c08c00edbe1fcf2e6d21
mukeshkapse <mukeshkapse1832@gmail.com>
2022-08-19 13:42:35 +0530

    Added : Employee is Prasent or absent

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.employeewage;
 
-public class EmployeeWage {
-    public static void main(String[] args) {
+public class EmployeeWage
+{
+    public static void main(String[] args)
+    {
         System.out.println("Welcome To Emplyee Wage Computation Program ");
+        double empstatus = Math.floor(Math.random()*10) %2;
+        if(empstatus==0) {
+            System.out.println("empoyee is Prasent ");
+        }else {
+        System.out.println("employee is Absent ");
+        }
     }
 }
